f70a064863f1f53475d8f57132974554ab9cab4c
root <root@root.localdomain>
2017-08-22 20:26:01 +0300

    updating server.js

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.get('/todos/:id', (req, res) => {
     }
     Todo.findById(id).then((todo) => {
         if(!todo) {
-            return res.status(404).send({error: "anvild id 2"})
+            return res.status(404).send({error: "invalid id "})
         }
         res.send(todo);
     });
